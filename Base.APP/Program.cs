08fcf0352139f7297a0d0fbd79ac4d127f2a630f
Rafael Fernando <rafafaelrafithow@hotmail.com>
2023-07-09 07:16:52 -0300

    Adicionado APP

---
+++ Base.APP/Program.cs
@@ -0,0 +1,11 @@
+using Base.APP;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44316/") });
+
+await builder.Build().RunAsync();
