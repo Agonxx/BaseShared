fb0380879ad9913636441036eba6a193b0b2dd26
Rafael Fernando <rafafaelrafithow@hotmail.com>
2023-07-09 21:50:23 -0300

    Login parcialmente feito

--- Base.APP/Program.cs
+++ Base.APP/Program.cs
@@ -1,4 +1,8 @@
 using Base.APP;
+using Base.APP.Services.Autenticacao;
+using Base.APP.Services.Interfaces;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +10,19 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44316/") });
+builder.Services.AddHttpClient("Base.API", opts =>
+{
+    opts.BaseAddress = new Uri("https://localhost:44316/");
+});
+
+//Services do Sistema Base
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+
+//Services das Classes
+
 
 await builder.Build().RunAsync();
