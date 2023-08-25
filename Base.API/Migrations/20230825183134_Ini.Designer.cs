69be5332aa9a33bcdd0432b6534c1d2fe78c36ce
Rafael.Santos <supersoftdev01@outlook.com>
2023-08-25 15:45:55 -0300

    s

--- Base.API/Migrations/20230825183134_Ini.Designer.cs
+++ Base.API/Migrations/20230825183134_Ini.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Base.API.Migrations
 {
     [DbContext(typeof(BaseContext))]
-    [Migration("20230825044405_Ini")]
+    [Migration("20230825183134_Ini")]
     partial class Ini
     {
         /// <inheritdoc />
